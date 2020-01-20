9a1d82b2585eaf7c7b76a9f4a618bc0b1b79f8e1
Luiz Teixeira <luiz.teixeira5@fatec.sp.gov.br>
2020-01-20 20:16:25 -0300

    Adicionado projeto

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://luizhdt:10021948@cnodestr0-reszs.gcp.mongodb.net/rocketseat?retryWrites=true&w=majority',{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,    
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+//Query Params: request.query(Filtros, ordenação, paginação, ...)
+//Route Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body(Dados para a criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+
+
+app.listen(3333)
---
+++ frontend/src/App.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+import api from './services/api'
+import DevItem from './components/DevItem'
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './main.css'
+
+//Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
+//Propriedade: Informações que um componentes PAI passa o componente FILHO
+//Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+function App() {
+  const [devs, setDevs] = useState([])
+  const [github_username, setGithubUsername] = useState('')
+  const [techs, setTechs] = useState('')
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+
+      setDevs(response.data)
+    }
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+    console.log(response.data)
+    setGithubUsername('')
+    setTechs('')
+
+    setDevs([...devs, response.data])
+  }
+
+  return (
+    <div id='app'>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor='github_username'>Usuário do Github</label>
+            <input 
+              name='github_username' 
+              id='github_username' 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-block">
+            <label htmlFor='techs'>Tecnologias</label>
+            <input 
+              name='techs' 
+              id='techs' 
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+              <div className="input-block">
+                <label htmlFor='latitude'>Latitude</label>
+                <input 
+                  type='number' 
+                  name='latitude' 
+                  id='latitude' 
+                  required 
+                  value={latitude}
+                  onChange={e => setLatitude(e.target.value)}
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor='longitude'>Longitude</label>
+                <input 
+                  type='number' 
+                  name='longitude' 
+                  id='longitude' 
+                  required 
+                  value={longitude}
+                  onChange={e => setLongitude(e.target.value)}
+                />
+              </div>
+          </div>
+          <button type='submit'>Salvar</button>         
+        </form>
+        
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+        </ul>
+      </main>
+    </div>
+  )
+}
+
+export default App;
